9c7417b0d36137a3d25383146fee0b0f2d1244be
RithickRavisubramanian <rithickr459@gmail.com>
2023-05-23 10:54:53 +0530

    Day-1

--- java 22-05-2023/MyClass.java
+++ java 22-05-2023/MyClass.java
@@ -1,32 +1,27 @@
-import java.lang.*;
-
-class MyClass
-{
-       static int y;
-       int z=79;
-       public static void main(String[] args)
-       {
-               int x=89;
-               System.out.println("Welcome" +x);
-                System.out.println("y =" +y);
-                MyClass mc = new MyClass();
-                System.out.println("z =" +mc.z);
-             
-        }
-} 
+package com.dal;
 
 
+public class MyClass {
+	
 
+	void printName() {
+		System.out.println("rithick");
+	}
+	
+	void add() {
+		int a = 6;
+		int b = 8;
+		System.out.println(a+b);
+	}
+	
+	 public static void main(String args[]) {
+		 MyClass pn = new MyClass();
+		 MyClass va = new MyClass();
+		 pn.printName();
+		 va.add();
 
-
-
-
-
-
-
-
-
-
+ }};
+	       
 
 
 
