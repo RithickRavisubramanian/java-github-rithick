40649461169b09e4710e520b08acdf0016b949ac
RithickRavisubramanian <rithickr459@gmail.com>
2023-05-23 10:48:58 +0530

    first commit

---
+++ java 22-05-2023/MyClass.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+
+class MyClass
+{
+       static int y;
+       int z=79;
+       public static void main(String[] args)
+       {
+               int x=89;
+               System.out.println("Welcome" +x);
+                System.out.println("y =" +y);
+                MyClass mc = new MyClass();
+                System.out.println("z =" +mc.z);
+             
+        }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
