8c2b63fc1772bf2b286ba0a50aac960597ccd540
RithickRavisubramanian <rithickr459@gmail.com>
2023-05-23 11:10:47 +0530

    second commit

--- java 22-05-2023/MyClass.java
+++ java 22-05-2023/MyClass.java
@@ -8,19 +8,37 @@ public class MyClass {
 		System.out.println("rithick");
 	}
 	
+	int a= 20;
+	
+	
 	void add() {
 		int a = 6;
 		int b = 8;
 		System.out.println(a+b);
 	}
+	 void mul() {
+		int a = 67;
+		int b = 43;
+		System.out.println(a*b);
+	 }
+		
 	
 	 public static void main(String args[]) {
 		 MyClass pn = new MyClass();
 		 MyClass va = new MyClass();
+		 MyClass vm = new MyClass();
 		 pn.printName();
 		 va.add();
-
+		 vm.mul();
+		 System.out.println(pn.a);
+		 
+		 
  }};
+	       
+
+
+
+
 	       
 
 
